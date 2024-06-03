e7632fbef7869f023ff4be2c2ac5ce733f9ab0b5
jw5250 <jw5250@rit.edu>
2024-06-03 12:21:36 -0400

    Removed LIL_ENDIAN macro option in dataType.h

--- dataType.h
+++ dataType.h
@@ -1,18 +1,21 @@
 #ifndef DATATYPE_H_INCLUDED
 #define DATATYPE_H_INCLUDED
+
+//Assumes machine running this is little endian.
+
 typedef unsigned char byte;
 
 typedef union {
 	unsigned short WORD;
 	struct{
-		#ifdef LIL_ENDIAN
-			byte UPPER;
-			byte LOWER;
-		#else
-			byte LOWER;
-			byte UPPER;
-		#endif
-	}BYTE;
 
+
+			//As addresses increase in big endian system, byte significance increases.
+			//Lower byte address, then upper byte address
+			byte LOWER;
+			byte UPPER;
+
+	}BYTE;
+	//Chip 8 is big endian.
 } word;
 #endif
