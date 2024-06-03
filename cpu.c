e219eb73491ff928fea44150d028b8aa87c52c15
jw5250 <jw5250@rit.edu>
2024-06-03 13:26:30 -0400

    Fixed overflow flags to now set after arithmetic operations. Fixed expressions in lsh and rsh that were supposed to set overflow flags.

--- cpu.c
+++ cpu.c
@@ -10,15 +10,12 @@
 #include <stdbool.h>
 #define STACKSIZE 48
 #define STACKADDR 0x0
+
+//Quirk notes:
+	//Does x = x >> 1 and x << 1 instead of the following:
+		//x = y << 1 and x = y >> 1
 //inline:Larger executable, but functions declared with it don't need to be pushed onto the stack and popped (alongside result).
 //I could rewrite the giant if else statement into a jump table. Is it worth it though?
-
-//Writing the actual "hardware":
-	//Store the actual interpreter in the memory
-	//Needed instructions:
-	//draw functions
-	//key operations
-	//delay and timer functions
 
 void initCpu(){
 	cpu.pc.WORD = STARTADDR;
@@ -33,11 +30,10 @@ void initCpu(){
 static inline void addReg(int reg1, byte b1){
 	cpu.reg[reg1] += b1;
 }
-//8XY5
-static inline void subCarry(int reg1, int reg2){
-	cpu.reg[0xf] = (cpu.reg[reg1] > cpu.reg[reg2]);
-	cpu.reg[reg1] -= cpu.reg[reg2];
-}
+
+
+
+
 //8XY1
 static inline void or(int reg1, int reg2){
 	cpu.reg[reg1] |= cpu.reg[reg2];
@@ -47,34 +43,71 @@ static inline void and(int reg1, int reg2){
 	cpu.reg[reg1] &= cpu.reg[reg2];
 }
 
-//8XY4
-//Handles carry flags
-static inline void addCarry(int reg1, int reg2){
-	cpu.reg[0xf] = (~(cpu.reg[reg1])) < cpu.reg[reg2];
-	
-	//printf("Registers:%d, %d\n", reg1, reg2);
-	cpu.reg[reg1] += cpu.reg[reg2];
-}
 //8XY3
 static inline void xor(int reg1, int reg2){
 	cpu.reg[reg1] ^= cpu.reg[reg2];
 }
-//8XYE
-//Shift left contents in a register by 1
-static inline void lsh(int reg1){
-	cpu.reg[0xf] = ((cpu.reg[reg1] & 0x8000) != 0);
-	cpu.reg[reg1] <<= 1;
+
+//8XY4
+//Handles carry flags
+//If reg f would have been modified, its value is overridden by the carry flag.
+static inline void addCarry(int reg1, int reg2){
+	//cpu.reg[0xf] = ((byte)(~(cpu.reg[reg1]))) < (byte)(cpu.reg[reg2]);
+	//Note: If I don't cast these as bytes the operator won't treat the results as such.
+
+	//printf("Registers:%d, %d\n", reg1, reg2);
+	cpu.reg[reg1] += cpu.reg[reg2];
+	//When overflow causes a wrap around, result will always be less than the least valued register.
+	cpu.reg[0xf] = (byte)(cpu.reg[reg1]) < (byte)cpu.reg[reg2];
 }
+
+//8XY5
+//If reg f would have been modified, its value is overridden by the carry flag.
+static inline void subCarry(int reg1, int reg2){
+	byte a = cpu.reg[reg1];
+	byte b = cpu.reg[reg2];
+
+	cpu.reg[reg1] -= cpu.reg[reg2];
+	cpu.reg[0xf] = (a >= b);
+	//printf("SubCarry\nReg %d:%d\n", reg1, cpu.reg[reg1]);
+}
+
+//8XY7
+static inline void subCarryInvert(int reg1, int reg2){
+	byte a = cpu.reg[reg1];
+	byte b = cpu.reg[reg2];
+
+	cpu.reg[reg1] = cpu.reg[reg2] - cpu.reg[reg1];
+	cpu.reg[0xf] = (a <= b);
+	//printf("SubCarry\nReg %d:%d\n", reg1, cpu.reg[reg1]);
+}
+
+
+
 //8XY6
 //Shift right contents in a register by 1
+//If reg f would have been modified, its value is overridden by the carry flag.
 static inline void rsh(int reg1){
-	cpu.reg[0xf] = cpu.reg[reg1] & 0x0001;
+	byte a = (cpu.reg[reg1]) & 0x01;
 	cpu.reg[reg1] >>= 1;
+	cpu.reg[0xf] = a;
 }
+
+//8XYE
+//Shift left contents in a register by 1
+//If reg f would have been modified, its value is overridden by the carry flag.
+static inline void lsh(int reg1){
+	printf("Left shift register %d before:%d\n", reg1, cpu.reg[reg1]);
+	byte a = ( ((cpu.reg[reg1]) & 0x80) != 0);//This doesn't work.
+	printf("Overflow flag:%d\n", a);
+	cpu.reg[reg1] <<= 1;
+	cpu.reg[0xf] = a;
+	printf("Left shift register %d:%d\n", reg1, cpu.reg[reg1]);
+}
+
 //1NNN
 static inline void jmp(word nextInstr){
-	//Debug message below.
-	printf("Jumping\n");
+	//printf("Jumping\n");
 	cpu.pc.WORD = nextInstr.WORD & 0x0fff;
 }
 //BNNN
@@ -83,7 +116,7 @@ static inline void jmpOff(word w1){
 }
 //ANNN
 static inline void setIndex(word w1){
-	printf("set index");
+	//printf("set index\n");
 	cpu.i.WORD = w1.WORD & (0x0fff);
 }
 
@@ -126,7 +159,7 @@ static inline void bne(byte b1, byte b2){
 
 //6XNN
 static inline void load(int reg1, byte b1){
-	printf("Register and byte:%d, %d\n", reg1, b1);
+	//printf("Register and byte:%d, %d\n", reg1, b1);
 	cpu.reg[reg1] = b1;
 }
 
@@ -155,7 +188,7 @@ static inline void bcdEncode(int reg1){
 
 //DXYN
 static inline void draw(int regX, int regY, int nBytes){
-	printf("Drawing\n");
+	//printf("Drawing\n");
 	int i = 0;
 	byte bitMasks[BYTE_LEN_IN_BITS] = {128, 64, 32, 16, 8, 4, 2, 1};
 	bool collisionOccuredAtSomeTime = false;
@@ -181,6 +214,7 @@ static inline void draw(int regX, int regY, int nBytes){
 		i++;
 	}
 	cpu.reg[0xF] = collisionOccuredAtSomeTime;
+	updateScreen();
 }
 
 //FX55
@@ -223,7 +257,7 @@ static inline void arithmeticOperations(int reg1, int reg2, int id){
 	}else if(id == 5){
 		subCarry(reg1, reg2);
 	}else if(id == 7){
-		subCarry(reg2, reg1);
+		subCarryInvert(reg1, reg2);
 	}
 }
 
@@ -271,6 +305,7 @@ void cpuLoop(){
 		} else if( ((firstNibble | secondNibble) | thirdNibble) == 0x00E0 ){
 			//Calls the screen refreshing routine.
 			 clearScreen();
+			 updateScreen();
 		}
 	}else if(opcode == 1){
 		//jump to address NNN
@@ -314,11 +349,11 @@ void cpuLoop(){
 		}
 		bitWiseOperations((firstNibble >> 8), (secondNibble >> 4), thirdNibble);
 		arithmeticOperations(firstNibble >> 8, secondNibble >> 4, thirdNibble);
-		shiftOperations(cpu.reg[(firstNibble >> 8)], thirdNibble);
+		shiftOperations((firstNibble >> 8), thirdNibble);
 	}else if(opcode == 9){
 		//skip if not equal (register)
 		if(thirdNibble == 0){
-			bne( cpu.reg[firstNibble >> 8],cpu.reg[secondNibble >> 4] );
+			bne( cpu.reg[firstNibble >> 8], cpu.reg[secondNibble >> 4] );
 		}	
 	}else if(opcode == 0xA){
 		word address;
@@ -343,11 +378,11 @@ void cpuLoop(){
 		}else if( currentInstruction.BYTE.LOWER == 0x55 ){
 			dumpReg( (firstNibble >> 8) );
 		}else if( currentInstruction.BYTE.LOWER == 0x65 ){
-			printf("Loading registers\n");
+			//printf("Loading registers\n");
 			loadReg( (firstNibble >> 8) );
 		}
 	}else{
-		fprintf(stdout, "Uh oh\n");
+		fprintf(stderr, "Uh oh\n");
 	}
 }
 
